2480f795a526da767a3c838b51d1375d72dae60b
Stray Petal <150872093+strayPetal@users.noreply.github.com>
2024-01-25 11:09:35 +1300

    renames that won't make me make another release

--- src/main/java/deckedoutdroppers/DeckedOutDroppersMod.java
+++ src/main/java/deckedoutdroppers/DeckedOutDroppersMod.java
@@ -21,51 +21,51 @@ public class DeckedOutDroppersMod {
     public static final Block NEVER_REPLENISH_MARKER = Blocks.PETRIFIED_OAK_SLAB;
     
     public static final DispenseActionConditions REPLENISH_CONDITIONS = new DispenseActionConditions(List.of(
-            List.of(itemIs(Items.IRON_NUGGET),
-                    customModelValueIs(1,    // Coin
-                                       2,    // Crown
-                                       3,    // Frost Ember
-                                       201,  // Level 2 Key
-                                       203,  // Level 3 Key
-                                       209,  // Level 4 Key
-                                       208,  // The Bomb
-                                       6,    // Victory Tome
-                                       42,   // Hood of Aw'Yah
-                                       43,   // Axe of the Screamin' Void
-                                       17,   // Multi-Grain Waffle
-                                       33,   // Shades of the Dog
-                                       30,   // Papa's Slippers
-                                       28,   // Jar of Speedy Slime
-                                       29,   // Tome of the Hills
-                                       34,   // Death Loop
-                                       32,   // Pearl of Cleansing
-                                       13,   // Horn of the G.O.A.T.
-                                       12,   // Chisel of the Undead Sculptress
-                                       40,   // Butcher's Apron
-                                       18,   // Hypnotic Bandana
-                                       35,   // Wand of Gorgeousness
-                                       41,   // Knight's Helm
-                                       20,   // Bionic Eye of Doom
-                                       31,   // The Hidden Stache
-                                       15,   // Goggles of Symmetry
-                                       19,   // Golden Eye
-                                       10,   // Pocket Watch of Shreeping
-                                       39,   // An Old Friend's Pickaxe
-                                       16,   // Gem of Greatness
-                                       11,   // CF-135
-                                       44,   // Staff of the Pink Shepherd
-                                       14,   // The Slab
-                                       38,   // The Skadoodler
-                                       36,   // Mug of the Dungeon Master
-                                       37)), // The Master's Key
-            List.of(itemIs(Items.IRON_INGOT),
-                    customModelValueIs(2        // Rusty Repair Kit
+            List.of(itemTypeIs(Items.IRON_NUGGET),
+                    customModelDataIs(1,    // Coin
+                                      2,    // Crown
+                                      3,    // Frost Ember
+                                      201,  // Level 2 Key
+                                      203,  // Level 3 Key
+                                      209,  // Level 4 Key
+                                      208,  // The Bomb
+                                      6,    // Victory Tome
+                                      42,   // Hood of Aw'Yah
+                                      43,   // Axe of the Screamin' Void
+                                      17,   // Multi-Grain Waffle
+                                      33,   // Shades of the Dog
+                                      30,   // Papa's Slippers
+                                      28,   // Jar of Speedy Slime
+                                      29,   // Tome of the Hills
+                                      34,   // Death Loop
+                                      32,   // Pearl of Cleansing
+                                      13,   // Horn of the G.O.A.T.
+                                      12,   // Chisel of the Undead Sculptress
+                                      40,   // Butcher's Apron
+                                      18,   // Hypnotic Bandana
+                                      35,   // Wand of Gorgeousness
+                                      41,   // Knight's Helm
+                                      20,   // Bionic Eye of Doom
+                                      31,   // The Hidden Stache
+                                      15,   // Goggles of Symmetry
+                                      19,   // Golden Eye
+                                      10,   // Pocket Watch of Shreeping
+                                      39,   // An Old Friend's Pickaxe
+                                      16,   // Gem of Greatness
+                                      11,   // CF-135
+                                      44,   // Staff of the Pink Shepherd
+                                      14,   // The Slab
+                                      38,   // The Skadoodler
+                                      36,   // Mug of the Dungeon Master
+                                      37)), // The Master's Key
+            List.of(itemTypeIs(Items.IRON_INGOT),
+                    customModelDataIs(2        // Rusty Repair Kit
                     )),
-            List.of(itemIs(Items.PUMPKIN),
-                    customNameColorIs("dark_purple" // Halloween Pumpkin
+            List.of(itemTypeIs(Items.PUMPKIN),
+                    nameFormatColorIs("dark_purple" // Halloween Pumpkin
                     )),
-            List.of(itemIs(Items.COMPASS),
-                    customNameColorIs("green",     // 1 Easy
+            List.of(itemTypeIs(Items.COMPASS),
+                    nameFormatColorIs("green",     // 1 Easy
                                       "yellow",    // 1 Medium
                                       "#fed83d",   // 2 Medium
                                       "gold",      // 2 Hard, 3 Hard
@@ -73,8 +73,8 @@ public class DeckedOutDroppersMod {
                                       "dark_red",  // 4 Deadly
                                       "#3ab3da"    // 4 Deepfrost
                     )),
-            List.of(itemIs(Items.FILLED_MAP),
-                    mapIdIs(976     // In-Game UI Map
+            List.of(itemTypeIs(Items.FILLED_MAP),
+                    mapIs(976     // In-Game UI Map
                     ))
     ));
     
@@ -94,19 +94,19 @@ public class DeckedOutDroppersMod {
         return false;
     }
     
-    private static Function<DispenseAction, Boolean> itemIs(Item itemType) {
+    private static Function<DispenseAction, Boolean> itemTypeIs(Item itemType) {
         return da -> da.getItemType() == itemType;
     }
     
-    private static Function<DispenseAction, Boolean> customModelValueIs(Integer... customModelValues) {
-        return da -> isOneOf(da.getItemNbt().getInt("CustomModelData"), customModelValues);
+    private static Function<DispenseAction, Boolean> customModelDataIs(Integer... customModelDataValues) {
+        return da -> isOneOf(da.getItemNbt().getInt("CustomModelData"), customModelDataValues);
     }
     
-    private static Function<DispenseAction, Boolean> customNameColorIs(String... color) {
+    private static Function<DispenseAction, Boolean> nameFormatColorIs(String... color) {
         return da -> isOneOf(da.getItemNbt().getCompound("NameFormat").getString("color"), color);
     }
     
-    private static Function<DispenseAction, Boolean> mapIdIs(Integer... mapIds) {
-        return da -> isOneOf(da.getItemNbt().getInt("map"), mapIds);
+    private static Function<DispenseAction, Boolean> mapIs(Integer... map) {
+        return da -> isOneOf(da.getItemNbt().getInt("map"), map);
     }
 }
